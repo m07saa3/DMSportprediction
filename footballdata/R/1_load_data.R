ba5331b840cb6dcb34a42b0a295ccc593ab3cbe3
andrei shabanov <shabanovmail@gmail.com>
2015-04-05 18:09:44 +0300

    change load data

--- footballdata/R/1_load_data.R
+++ footballdata/R/1_load_data.R
@@ -11,7 +11,9 @@ get.data <- function(league = "B1") {
   path.file = file.path("..", "data_raw", "football_data_co_uk", paste0(league, ".csv"))
   dataset <- read.csv(path.file, stringsAsFactors = FALSE)
   dataset$Date <- dmy(dataset$Date)
-  mask <- year(dataset$Date) > 2030 #ke-ke-ke :)
-  dataset$Date[mask] <- dataset$Date[mask] - years(100)
+  tail <- year(dataset$Date)%% 2000 #ke-ke-ke :)
+  mask <- tail < 50
+  year(dataset$Date)[mask] <- 2000 + tail[mask]
+  year(dataset$Date)[!mask] <- 1900 + tail[!mask]
   dataset
 }
